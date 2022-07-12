df8b5c4161ed879746111cd9a17c0588725eb8eb
Sithumameesha <sithumameesha76@gmail.com>
2022-07-12 12:04:52 +0530

    Add Student Model

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class StudentFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtNic;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public JFXTextField txtEmail;
+    public Button btnDelete;
+    public Button btnSave;
+    public TableView tblStudent;
+
+    public void initialize(){
+
+    }
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SaveOnAction(ActionEvent actionEvent) {
+        SaveStudent();
+    }
+
+    private void SaveStudent() {
+
+    }
 }
